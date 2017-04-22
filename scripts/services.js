4e46295652a21e7350ccbaa78b1d75045382c621
Vadim <korobeinikovv@mail.ru>
2017-04-22 03:19:00 +0300

    correct init zoom

--- scripts/services.js
+++ scripts/services.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	var map = L.map('map', {
 			center: [36.98, -120.12],
-			zoom: 8
+			zoom: 3
 	});
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia2FuZ2Fyb29vIiwiYSI6ImNqMXNkbWl5NjAwMWUzMnJ6eDdqbWV1dnAifQ.yHRhnXJ0ek-a6DrU5-GjEQ', {
@@ -13,14 +13,15 @@ window.onload = function() {
 
 	var processData = function(data) {
 		data.forEach(function(item){
-			wkt.read(item.toString().split(";")[1]);
+			var arr = item.toString().split(";");
+			wkt.read(arr[1]);
 			var coords = wkt.components[0];
 			
 			L.circle([coords.x, coords.y], {
 				color: 'red',
 				fillColor: '#f03',
 				fillOpacity: 0.5,
-				radius: 1000
+				radius: arr[2]
 			}).addTo(map);
 		});
 	}
